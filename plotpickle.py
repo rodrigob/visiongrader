2ed2299983c1fa4fa6810b9410603e2343955d04
myrhev <myrhev32@localhost.localdomain>
2010-06-08 14:02:05 -0400

    Improved printer

--- plotpickle.py
+++ plotpickle.py
@@ -2,8 +2,19 @@ import pylab
 import cPickle
 import sys
 
+n_colors = 6
+colors = ['b', 'g', 'c', 'm', 'y', 'k']
+xmin = 0.003
+xmax = 200
+ymin = 0.04
+ymax = 1.01
 i = 0
-def plot(arg):
+def get_y1(points):
+    return int(float([-a[1] for a in points if a[0] <= 1][-1]) * 100.)
+
+toplot = []
+
+def plot(arg, main = False):
     global i
     f = open(arg, "r")
     points = cPickle.load(f)
@@ -13,22 +24,43 @@ def plot(arg):
         label = label[:label.rfind(".")]
     if label.find("/") != -1:
         label = label[label.rfind("/")+1:]
-    if i < 7:
+    if label == "eblearn":
+        label = "U+_tanh R+_tanh"
+    label += " (%d%%)"%(get_y1(points),)
+    color = colors[i%6]
+    width = 1
+    if main == True:
         style = "-"
-    elif i < 14:
+        color = "r"
+        width = 2
+        i -= 1
+    elif i < n_colors:
+        style = "-"
+    elif i < 2*n_colors:
         style = "--"
-    elif i < 21:
+    elif i < 3*n_colors:
         style = ":"
     else:
         style = "-."
     i += 1
-    pylab.loglog([a[0] for a in points], [- a[1] for a in points], style, label = label)
+    toplot.append([[-get_y1(points), main], [a[0] for a in points],
+                   [- a[1] for a in points], style,
+                   color, label, width])
+
+def plot_bar(x):
+    pylab.plot([x, x], [ymin, ymax], color = 'k')
 
 for arg in [a for a in sys.argv[1:] if a.find("eblearn") == -1]:
     plot(arg)
 for arg in [a for a in sys.argv[1:] if a.find("eblearn") != -1]:
-    plot(arg)
-pylab.legend(loc=3)
+    plot(arg, True)
+toplot.sort()
+for p in toplot:
+    pylab.loglog(p[1], p[2], p[3], color = p[4], label = p[5], linewidth = p[6])
+plot_bar(1.0)
+
+pylab.legend(loc=4)
 pylab.xlabel("False positives per image")
 pylab.ylabel("Miss rate")
+pylab.axis(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax)
 pylab.show()
