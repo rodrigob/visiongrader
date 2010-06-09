56cd8cc64addbab9d5150da6ad020ed41d9fe4c8
Michael Mathieu <myrhev@users.sourceforge.net>
2010-06-09 14:49:01 -0400

    Added GPL license

--- dataset.py
+++ dataset.py
@@ -1,3 +1,23 @@
+# Copyright (C) 2010 Michael Mathieu <michael.mathieu@ens.fr>
+# 
+# This file is part of visiongrader.
+# 
+# visiongrader is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# visiongrader is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with visiongrader.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Authors :
+#  Michael Mathieu <michael.mathieu@ens.fr>
+
 import copy
 from slist import slist
 
